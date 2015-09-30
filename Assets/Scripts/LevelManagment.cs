7bc64aa6cd151d149fadd2f82c5f34ec31f00743
Thais Dias <dragonssaur@gmail.com>
2015-09-30 17:11:01 -0300

    v5.1
    I'm almost done

---
+++ Assets/Scripts/LevelManagment.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManagment : MonoBehaviour {
+
+	public int level;
+
+	public void levelManager () {
+		Application.LoadLevel (level); 	
+	
+	}
+
+	void Update() 
+	{
+		if (Input.GetKey (KeyCode.Escape)) {
+			Application.Quit();
+			
+		} 
+
+	} 
+}
