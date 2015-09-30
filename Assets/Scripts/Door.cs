aca92b6dedc90d4f4be839319fa00395798c19c5
Thais Dias <dragonssaur@gmail.com>
2015-09-30 16:21:05 -0300

    v5.0

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D colisor)
+	{
+
+		if (colisor.gameObject.tag == "Player" && colisor.gameObject.GetComponent<Character>().hasKey) {
+			PlayerPrefs.SetInt("lastLevel", Application.loadedLevel);
+			Application.LoadLevel("LevelComplete");
+		} 
+	
+	}
+}
---
+++ Assets/Scripts/LevelManagment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManagment : MonoBehaviour {
+
+	public int level;
+
+	public void levelManager () {
+		Application.LoadLevel (level); 	
+	}
+}
