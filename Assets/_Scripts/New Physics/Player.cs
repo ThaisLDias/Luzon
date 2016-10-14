97ccf7d67da92a7342bccaed38032f6011665b7f
Thais Dias <dragonssaur@gmail.com>
2016-10-14 14:59:56 -0300

    Really cool stuff

--- Assets/_Scripts/New Physics/Player.cs
+++ Assets/_Scripts/New Physics/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
@@ -27,81 +28,96 @@ public class Player : MonoBehaviour {
 	Vector3 velocity;
 	float velocityXSmoothing;
 
+	SpriteRenderer sprite;
 	Controller2D controller;
-
+	Animator anim;
 
 	float lastFacingDir = 1;
+
+
+	#region Foda-se
+
+	public bool hasGravity, hasWallJump;
+
+
+	#endregion
+
 
 	/**/
 
 	void Start() {
+		anim = GetComponent<Animator> ();
 		controller = GetComponent<Controller2D> ();
+		sprite = GetComponent<SpriteRenderer> ();
+	
 	}
 	void GravityCalculator(){
-		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
-		maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-		minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
-	}
-	void Move(){
-		#region FacingDirection
-		if (this.input.x != 0) {
-			this.transform.localScale = new Vector3 (input.x, 1, 1);
-			this.lastFacingDir = input.x;
-		} else {
-			this.transform.localScale = new Vector3 (this.lastFacingDir, 1, 1);
-
+		if (hasGravity) {
+			gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+			maxJumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+			minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
+	
 		}
-		#endregion
+	}
 
+	void Move(){
 		input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));	
+
+		if (input.x == -1) 
+			sprite.flipX = true;
+		
+		if (input.x == 1) 
+			sprite.flipX = false;
+		
+		
 	}
 	void WallJump(){
+		if(!hasWallJump){
 		int wallDirX = (controller.collisions.left) ? -1 : 1;
 
 		float targetVelocityX = input.x * moveSpeed;
 		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
 
 		bool wallSliding = false;
-		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0) {
-			wallSliding = true;
+			if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0) {
+				wallSliding = true;
 
-			if (velocity.y < -wallSlideSpeedMax) {
-				velocity.y = -wallSlideSpeedMax;
-			}
-
-			if (timeToWallUnstick > 0) {
-				velocityXSmoothing = 0;
-				velocity.x = 0;
-
-				if (input.x != wallDirX && input.x != 0) {
-					timeToWallUnstick -= Time.deltaTime;
+				if (velocity.y < -wallSlideSpeedMax) {
+					velocity.y = -wallSlideSpeedMax;
 				}
-				else {
+
+				if (timeToWallUnstick > 0) {
+					velocityXSmoothing = 0;
+					velocity.x = 0;
+
+					if (input.x != wallDirX && input.x != 0) {
+						timeToWallUnstick -= Time.deltaTime;
+					} else {
+						timeToWallUnstick = wallStickTime;
+					}
+				} else {
 					timeToWallUnstick = wallStickTime;
 				}
-			}
-			else {
-				timeToWallUnstick = wallStickTime;
-			}
 
 		}
 
-		if (Input.GetAxis("Vertical") >= 0) {
-			if (Input.GetAxisRaw("Vertical")> 0) {
-				if (wallSliding) {
-					if (wallDirX == input.x) {
-						velocity.x = -wallDirX * wallJumpClimb.x;
-						velocity.y = wallJumpClimb.y;
-					} else if (input.x == 0) {
-						velocity.x = -wallDirX * wallJumpOff.x;
-						velocity.y = wallJumpOff.y;
-					} else {
-						velocity.x = -wallDirX * wallLeap.x;
-						velocity.y = wallLeap.y;
+			if (Input.GetAxis ("Vertical") >= 0) {
+				if (Input.GetAxisRaw ("Vertical") > 0) {
+					if (wallSliding) {
+						if (wallDirX == input.x) {
+							velocity.x = -wallDirX * wallJumpClimb.x;
+							velocity.y = wallJumpClimb.y;
+						} else if (input.x == 0) {
+							velocity.x = -wallDirX * wallJumpOff.x;
+							velocity.y = wallJumpOff.y;
+						} else {
+							velocity.x = -wallDirX * wallLeap.x;
+							velocity.y = wallLeap.y;
+						}
 					}
-				}
-				if (controller.collisions.below) {
-					velocity.y = maxJumpVelocity;
+					if (controller.collisions.below) {
+						velocity.y = maxJumpVelocity;
+					}
 				}
 			}
 		}
@@ -122,11 +138,27 @@ public class Player : MonoBehaviour {
 
 	}
 
+	void AnimationHandler(){
+	
+		if (controller.collisions.below) {
+			anim.SetBool ("Grounded", true);
+		} else {
+			anim.SetBool ("Grounded", false);
+		}
+
+		bool asd = (input.y == 1) ? true : false;
+		anim.SetBool ("JumpButton", asd );
+	
+		anim.SetInteger ("Moving", Mathf.RoundToInt(input.x));
+	
+	}
 
 	void Update() {
+
 		GravityCalculator ();
 		Move ();
 		WallJump ();
+		AnimationHandler ();
 
 	}
 
