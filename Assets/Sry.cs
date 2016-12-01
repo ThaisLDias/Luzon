9e733da8faedb201c3dbc309bc7b4a25bd967143
Thais Dias <dragonssaur@gmail.com>
2016-12-01 02:39:40 -0200

    Bugs????
    Some bugs were fixed
    But they always come back

---
+++ Assets/Sry.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Sry : MonoBehaviour {
+
+	public void clickpass ()
+	{
+		SceneManager.LoadScene ("Level_8");
+	}
+}
